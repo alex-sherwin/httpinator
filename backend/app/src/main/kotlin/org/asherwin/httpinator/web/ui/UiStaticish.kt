f1b124b08cb2ea029aa46530dda14bd3a49ac0ef
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2023-08-26 08:38:56 -0400

    dynamic ui templating

--- backend/app/src/main/kotlin/org/asherwin/httpinator/web/ui/UiStaticish.kt
+++ backend/app/src/main/kotlin/org/asherwin/httpinator/web/ui/UiStaticish.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping("/ui")
+@RequestMapping("/uitest")
 class UiStaticish {
 
   @GetMapping(path = ["", "/", "/index.html"])
